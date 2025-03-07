df33440ec6b45fa1b8e89dbb26af21d445c63138
EdsonPetry <edsonlbpetry@gmail.com>
2025-03-07 17:43:58 -0500

    Created getters and setters for SavingsBucket class

--- backend/models/SavingsBucket.py
+++ backend/models/SavingsBucket.py
@@ -1,24 +1,62 @@
-class Bucket:
+class SavingsBucket:
 
-    def __init__(self, goal: int, deadline: str):
-        self._goal = goal
-        self._deadline = deadline
-        self._balance = 0
+    def __init__(self, id: int, user_id: int, name: str, goal_amount: int, current_amount: int, target_date: str, icon):
+        self._id = id
+        self._user_id = user_id
+        self._name = name
+        self._goal_amount = goal_amount
+        self._current_amount = current_amount
+        self._target_date = target_date
+        self._icon = icon
     
-    # 
     @property
-    def goal(self) -> int:
-        return self._goal
+    def id(self):
+        return self._id
     
-    @goal.setter
-    def goal(self, new_amount: int) -> None:
-        self._goal = new_amount
+    @property
+    def user_id(self):
+        return self._user_id
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
 
     @property
-    def deadline(self) -> str:
-        return self._deadline
+    def goal_amount(self):
+        return self._goal_amount
     
-    @deadline.setter
-    def deadline(self, new_deadline) -> None:
-        self._deadline = new_deadline
+    @goal_amount.setter
+    def goal_amount(self, new_goal_amount):
+        self._goal_amount = new_goal_amount
+    
+    @property
+    def current_amount(self):
+        return self._current_amount
+    
+    @current_amount.setter
+    def current_amount(self, new_current_amount):
+        self._current_amount = new_current_amount
+
+    @property
+    def target_date(self):
+        return self._target_date
+    
+    @target_date.setter
+    def target_date(self, new_target_date):
+        self._target_date = new_target_date
+    
+    @property
+    def icon(self):
+        return self._icon
+    
+    @icon.setter
+    def icon(self, new_icon):
+        self._icon = new_icon
+    
+
+
     
